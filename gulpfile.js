a3b1ee8269176d91945dfa3dee93686ba9e6c4ff
blisterrabbithutch <blisterrabbithutch@gmail.com>
2017-07-17 23:29:08 +0500

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -89,6 +89,7 @@ gulp.task("serve", function() {
     });
 
     gulp.watch("less/**/*.less", ["style"]);
+    gulp.watch("less/blocks/*.less", ["style"]);
     gulp.watch("img/*.svg").on("change", server.reload);
     gulp.watch("*.html", ["html:update"]);
 });
